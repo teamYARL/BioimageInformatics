29cbf31382534caa09dbed38f857a49bbcf3d075
Luigi Leung <lleung@cmu.edu>
2014-02-21 15:59:27 -0500

    Corrected file

--- getbackgroundinfo.m
+++ getbackgroundinfo.m
@@ -1,13 +1,13 @@
 function [ mean,standarddeviation ] = getbackgroundinfo( im )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-clear, close all
+close all
 I=imread(im);
 figure, imshow(I,[])
-rec=getrect
+rec=getrect;
 background=imcrop(I,rec);
-mean=mean2(background);
-standarddeviation=std2(background);
+mean=mean2(background)
+standarddeviation=std2(background)
 
 end
 
