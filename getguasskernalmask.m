78250dd07e26943afcfa7e2003f1462eb53cc73d
Alan-g-s <ashteyma@andrew.cmu.edu>
2014-02-21 15:57:38 -0500

    guass update

--- getguasskernalmask.m
+++ getguasskernalmask.m
@@ -6,7 +6,7 @@ mask=zeros(2*ceil(3*sig)+1,2*ceil(3*sig)+1)
 
 for i = -ceil(3*sig):ceil(3*sig)
     for j= -ceil(3*sig):ceil(3*sig)
-        mask(i+ceil(3*sig)+1,j+ceil(3*sig)+1)=exp(-(i^2+j^2)/(2*(sig)))
+        mask(i+ceil(3*sig)+1,j+ceil(3*sig)+1)=exp(-(i^2+j^2)/(2*(sig)^2))
     end
 end
  mask = mask/(sum(sum(mask)))
