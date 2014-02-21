c031265281197cf2394b4245a7ef9fd9088c96f6
Luigi Leung <lleung@cmu.edu>
2014-02-21 15:39:39 -0500

    Updated q2.1 for ;, and updated .gitignore to ignore .tif and other large files

--- getbackgroundinfo.m
+++ getbackgroundinfo.m
@@ -1,12 +1,13 @@
 function [ mean,standarddeviation ] = getbackgroundinfo( im )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-I=imread(im)
-imshow(I,[])
+clear, close all
+I=imread(im);
+figure, imshow(I,[])
 rec=getrect
-background=imcrop(I,rec)
-mean=mean2(background)
-standarddeviation=std2(background)
+background=imcrop(I,rec);
+mean=mean2(background);
+standarddeviation=std2(background);
 
 end
 
