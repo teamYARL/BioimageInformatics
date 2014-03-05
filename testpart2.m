b64d6930f6733107fad6d901e9b8bd7c54f52871
Luigi Leung <lleung@cmu.edu>
2014-03-05 18:20:21 -0500

    Comparing different Gaussian blur sigma values, no change in number of max/min/triangles.

--- testpart2.m
+++ testpart2.m
@@ -1,8 +1,25 @@
 clear all, close all
-imagePath = '2\images\\001_a5_002_t001.tif';
+
+%imagePath = '2\images\001_a5_002_t001.tif';    % Path format in Windows
+imagePath = '2/images/001_a5_002_t001.tif';     % Path format in OSX/Linux
 I = imread(imagePath);
+
 [bgMean, bgSD] = getbackgroundinfo(imagePath);
-gaussMask = getguasskernalmask(10);
-I2 = conv2(I, gaussMask, 'same');
+disp('Done: getbackgroundinfo')
+
+gaussMask = getguasskernalmask(100);
+disp('Done: getguasskernalmask')
+
+I2 = conv2(double(I), gaussMask, 'same');       % To Do: change I to double(I) in all functions
+disp('Done: conv2 Image with gaussMask')
+% Comparing the gaussMark with sig=1 vs. sig=10 vs. sig=100, there is no
+%   change in the number of local maxima or local minima or triangles.
+
 [localMax, localMin] = detectlocalmaxmin(imagePath, 3);
+disp('Done: detectlocalmaxmin')
+
 DT = triangulationproject(localMin);
+disp('Done: triangulationproject')
+
+
+disp('FINISHED!')
