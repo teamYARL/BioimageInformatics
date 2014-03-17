bdfb7c80bbdbf4b2d7a7168c435c89b02aa2a5a6
Yuxian Ruan <ruanyuxian2008@gmail.com>
2014-03-17 17:54:42 -0400

    Delete gaussian2D.m

--- gaussian2D.m
+++
@@ -1,9 +0,0 @@
-%% 2D gaussian fit equation
-% x is the vector of parameters for equation
-function [F] = gaussian2D(x, xydata)
-xdata = xydata(1);
-ydata = xydata(2);
-class(xdata)
-class(x)
-class(x(1))
-F = x(1)*exp(-((xdata-x(3))^2+(ydata-x(4))^2)/x(2)^2);
