f8d8d72a60e2866e7ebe92a82fb846e25050825e
yimingx <yimingx@andrew.cmu.edu>
2014-03-17 15:11:49 -0400

    Update tTest.m

--- tTest.m
+++ tTest.m
@@ -1,15 +1,18 @@
-function [ newlocalmax ] = tTest( localmax, mean, sigma, Q )
+function [ newlocalmax ] = tTest(image, localmax, mean, sigma, Q )
 
-%disp(localmax);
+%disp(sum(sum(localmax)));
 
 tmp = size(localmax);
 rol = tmp(1);
 col = tmp(2);
 
+tSet = [];
+
 for i = 1 : rol
     for j = 1 : col
         if (localmax(i,j) == 1) 
-            T = abs(mean)/sqrt((sigma^2)/3);
+            T = (abs(image(i,j) - mean))/(sqrt((sigma^2)/3));
+            tSet = [tSet T];
             if(T < Q)
                 localmax(i,j) = 0;
             end
@@ -18,5 +21,7 @@ for i = 1 : rol
 end
 
 newlocalmax = localmax;
-%disp(newlocalmax);
+%disp(tSet);
+%disp(sum(sum(newlocalmax)));
+
 end
