85670f8952239cda09b9633233ddc5635b06288d
ruanyuxian <ruanyuxian2008@gmail.com>
2014-03-17 17:56:43 -0400

    updated

--- gaussianfit.m
+++ gaussianfit.m
@@ -1,34 +1,30 @@
-function [x] = gaussianfit(img)
+function x = gaussianfit(xData,yData,zData)
 
 %% Condition the data
-a = size(img);
-m = a(1);
-n = a(2);
-xx = 1:n;  %vector for x coordinate
-yy = 1:m;  %vector for y coordinate
-zz = img;
-[xData, yData, zData] = prepareSurfaceData(xx, yy, zz);
-xyData = {xData,yData};
+[X, Y] = meshgrid(xData,yData);
+XY(:,:,1) = X;  % x coordinate
+XY(:,:,2) = Y;  % y coordinate
+
+%% Create objective function
+gaussian2D = @(x,XY) x(1)*exp(-((XY(:,:,1)-x(3)).^2/x(2))-((XY(:,:,2)-x(4)).^2/x(2)));
+
 
 %% Set up the startpoint
-[amp, index] = max(zData); % amp is the amplitude.
-x0 = xData(index); % first guess at the maximum
-y0 = yData(index); % first guess at the maximum
-sigma = 1;
-StartPoint = [amp, sigma, x0, y0];
+amp = max(max(zData)); % amp is the amplitude.
+[x0, y0] = find(zData==max(max(zData)));  % first guess for position at the maximum
+b = 50;
+StartPoint = [amp, b, x0, y0];
 
-%% Set up the lower and upper bound 
-xmax = max(xData)+2;
-ymax = max(yData)+2;
-xmin = min(xData)-2;
-ymin = min(yData)-2;
-Lower = [0, 0, xmin, ymin];
-Upper = [Inf,Inf, xmax, ymax]; % angles greater than 90 are redundant
+disp(StartPoint);
 
 %% perform the fitting
-%x is the fitting result for parameters
-x = lsqcurvefit(@gaussian2D,StartPoint,xyData,zData,Lower,Upper); 
-% F = x(1)*exp(-((xdata-x(3))^2+(ydata-x(4))^2)/x(2));
+% x is the fitting result for parameters
+x = lsqcurvefit(gaussian2D,StartPoint,XY,zData); 
+
+% show Fitted Surface
+Z = gaussian2D(x,XY); 
+surf(X, Y, Z)
+
 
 end
 
