3bfd64f84a7313546cc7fd49e2802f44ec063250
yimingx <yimingx@andrew.cmu.edu>
2014-03-17 15:13:19 -0400

    Update test2.4.m

--- test2.4.m
+++ test2.4.m
@@ -1,6 +1,6 @@
 clear all, close all
 
-imagePath = '2\images\001_a5_002_t001.tif';    % Path format in Windows
+imagePath = '2/images/001_a5_002_t001.tif';    % Path format in Windows
 %imagePath = '2/images/001_a5_002_t001.tif';     % Path format in OSX/Linux
 I = imread(imagePath);
 
@@ -18,10 +18,42 @@ disp('Done: conv2 Image with gaussMask')
 [localMax, localMin] = detectlocalmaxmin(imagePath, 3);
 disp('Done: detectlocalmaxmin')
 
-DT = triangulationproject(localMin);
-disp('Done: triangulationproject')
+%DT = triangulationproject(localMin);
+%disp('Done: triangulationproject')
 
-[newlocalMax] = tTest(localMax,bgMean,bgSD,10.0);  %set Q = 10.0
+
+
+[newlocalMax] = tTest(I,localMax,bgMean,bgSD,10.0);  %set Q = 10.0
 disp('Done: t test')
 
+%mark the new localmaxs
+imshow(I);
+hold on;
+
+tmp = size(newlocalMax);
+rol = tmp(1);
+col = tmp(2);
+
+x=[];
+y=[];
+%xx=[];
+%yy=[];
+for i = 1 : rol
+    for j = 1 : col
+        if (newlocalMax(i,j) == 1) 
+           x=[x i];
+           y=[y j]; 
+        end
+       % if (localMin(i,j) == 1)
+       %    xx=[xx i];
+        %   yy=[yy j];
+        %end
+    end
+end
+plot(y,x,'o','MarkerFaceColor','green','MarkerEdgeColor','green','MarkerSize',5);
+%plot(yy,xx,'o','MarkerFaceColor','red','MarkerEdgeColor','red','MarkerSize',1);
+
+
+
+%imshow(I);
 disp('FINISHED!')
