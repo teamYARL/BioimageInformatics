2b7656302bfe8d5c4ab7b746e1d85391ae2835f8
Alan-g-s <ashteyma@andrew.cmu.edu>
2014-03-17 21:20:18 -0400

    more stuff

--- testpart2.m
+++ testpart2.m
@@ -1,13 +1,13 @@
 clear all, close all
 
-%imagePath = '2\images\001_a5_002_t001.tif';    % Path format in Windows
-imagePath = '2/images/001_a5_002_t001.tif';     % Path format in OSX/Linux
+imagePath = '2\images\001_a5_002_t001.tif';    % Path format in Windows
+%imagePath = '2/images/001_a5_002_t001.tif';     % Path format in OSX/Linux
 I = imread(imagePath);
 
 [bgMean, bgSD] = getbackgroundinfo(imagePath);
 disp('Done: getbackgroundinfo')
 
-gaussMask = getguasskernalmask(100);
+gaussMask = getguasskernalmask(1.18);
 disp('Done: getguasskernalmask')
 
 I2 = conv2(double(I), gaussMask, 'same');       % To Do: change I to double(I) in all functions
