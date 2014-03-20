8d8d62ae3c7278b5d02d5eba81d1f4f52a1ed044
Alan-g-s <ashteyma@andrew.cmu.edu>
2014-03-20 16:55:40 -0400

    3.3

--- accuracyofsubpixel.m
+++ accuracyofsubpixel.m
@@ -2,6 +2,7 @@ function [ output_args ] = accuracyofsubpixel( im ,listmax )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+% this is part 3.3
 nummax=length(listmax)
 for i=1:nummax
     
