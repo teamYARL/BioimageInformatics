7702e60e20c90474dbb5b725267b0bcbe6e9edc0
yimingx <yimingx@andrew.cmu.edu>
2014-03-20 20:03:18 -0400

    Display the result through 2.1 to 3.2

--- test32.m
+++ test32.m
@@ -1,61 +1,97 @@
 clear all, close all
 
-%imagePath = 'images/001_a5_002_t001.tif';    % Path format in Windows
-imagePath = '2/images/001_a5_002_t001.tif';     % Path format in OSX/Linux
+imagePath = 'images\001_a5_002_t001.tif'; % Path format in Windows
+%imagePath = '2/images/001_a5_002_t001.tif'; % Path format in OSX/Linux
 I = imread(imagePath);
 
 [bgMean, bgSD] = getbackgroundinfo(imagePath);
 disp('Done: getbackgroundinfo')
 
-gaussMask = getguasskernalmask(100);
+gaussMask = getguasskernalmask(1.18);
 disp('Done: getguasskernalmask')
 
-I2 = conv2(double(I), gaussMask, 'same');       % To Do: change I to double(I) in all functions
+I2 = conv2(double(I), gaussMask, 'same'); % To Do: change I to double(I) in all functions
 disp('Done: conv2 Image with gaussMask')
 % Comparing the gaussMark with sig=1 vs. sig=10 vs. sig=100, there is no
-%   change in the number of local maxima or local minima or triangles.
+% change in the number of local maxima or local minima or triangles.
 
-[localMax, localMin] = detectlocalmaxmin(imagePath, 3);
+figure, imshow(I2,[])
+
+[localMax, localMin] = detectlocalmaxmin(I2, 5);
 disp('Done: detectlocalmaxmin')
 
+di=size(I2);
+ro=di(1);
+co=di(2);
+
+figure,
+imshow(I,[])
+x=[];
+y=[];
+for i =1:ro
+    for j=1:co
+        if localMax(i,j)==1
+            x=[x,i];
+            y=[y,j];
+        end
+    end
+end
+
+hold on
+plot (y,x,'o','MarkerEdgeColor','red')
+%imshow(localMax,[])
+hold off
+
+figure,
+imshow(I,[])
+x=[];
+y=[];
+for i =1:ro
+    for j=1:co
+        if localMin(i,j)==1
+            x=[x,i];
+            y=[y,j];
+        end
+    end
+end
+hold on
+plot (y,x,'o','MarkerEdgeColor','green')
+%imshow(localMin,[])
+hold off
+
+[DT,x,y] = triangulationproject(localMin);
+disp('Done: triangulationproject')
+
+figure, triplot(DT)
+
+[ newlocalmax ] = tTest(I2, localMax, localMin, bgSD, 4.0 );  % Q = 4.0
+disp('Done: T-test Filtering of maxima')
+
+figure,
+imshow(I,[])
+hold on
+X=[];
+Y=[];
+for i =1:ro
+    for j=1:co
+        if newlocalmax(i,j)==1
+            X=[X,i];
+            Y=[Y,j];
+        end
+    end
+end
+plot(Y, X,'red+');
+
+%imshow(newlocalmax,[])
+%hold off
 
 
-
-
-%DT = triangulationproject(localMin);
-%disp('Done: triangulationproject')
-
-
-
-[newlocalMax] = tTest(I,localMax,localMin,bgSD,4.0);  %set Q = 4.0
-disp('Done: t test')
 
 %mark the new localmaxs
 
-imshow(I,[]);
-hold on
 
-tmp = size(newlocalMax);
-rol = tmp(1);
-col = tmp(2);
 
-x=[];
-y=[];
-%xx=[];
-%yy=[];
-for i = 1 : rol
-    for j = 1 : col
-        if (newlocalMax(i,j) == 1) 
-           x=[x i];
-           y=[y j]; 
-        end
-       
-    end
-end
-plot(y,x,'o','MarkerFaceColor','green','MarkerEdgeColor','green','MarkerSize',1);
-%plot(yy,xx,'o','MarkerFaceColor','red','MarkerEdgeColor','red','MarkerSize',1);
-
-[result,num] = interp(newlocalMax,I);
+[result, num, centerX, centerY] = interp(newlocalmax,I, bgMean);
 
 disp('Done: interptation');
 %disp(XX);
@@ -73,6 +109,13 @@ disp('Done: interptation');
 %end
 
 disp('Done: gaussian fitting');
+
+figure,
+imshow(I,[])
+hold on
+
+plot(centerX, centerY,'green+');
+
 
 %imshow(I);
 disp('FINISHED!')
