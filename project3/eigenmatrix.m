184f13931286e41e73a4189c4f4fb9629e9e1311
yimingx <yimingx@andrew.cmu.edu>
2014-04-02 15:59:16 -0400

    Update eigenmatrix.m

--- project3/eigenmatrix.m
+++ project3/eigenmatrix.m
@@ -2,12 +2,15 @@
 
 % H is the output of hessianmatrix function
 % E is the matrix of corresponding eigen vectors
-function [ E ] = eigenmatrix(H)
+function [ E , eigNoiseMatrix ] = eigenmatrix(H)
 
 % get size of H
 [m,n] = size(H);
 
 E = cell(m,n);
+
+%initialize the mark matrix
+eigNoiseMatrix = zeros(m,n);
 
 for i = 1:m
     for j=1:n
@@ -16,9 +19,17 @@ for i = 1:m
         eigval = diag(eigval);
         % find the position of max eigen value
         col = find(eigval==max(eigval(:)));
+        %if the eigval is smaller than 0, it may be a noise and we mark it
+        if(max(eigval(:)) <= 0)
+            eigNoiseMatrix(i,j) = 1;
+        end
+        
         % find the corresponding eigen vector for this max eigen value
         E{i,j} = eigvect(:,col);
     end
 end
+
+%disp(eigNoiseMatrix);
         
+          
         
