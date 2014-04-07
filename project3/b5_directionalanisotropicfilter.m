885e099461fb2bc39ff9a335ea64eca583b64bf8
Luigi Leung <lleung@cmu.edu>
2014-04-07 11:11:14 -0400

    Correcting b5

--- project3/b5_directionalanisotropicfilter.m
+++ project3/b5_directionalanisotropicfilter.m
@@ -1,4 +1,5 @@
-function [ filteredImage ] = b5_directionalanisotropicfilter( im, kappa, lambda )
+function [ filteredImage ] = b5_directionalanisotropicfilter( im, sig_long, sig_lat, phi )
+%function [ filteredImage ] = b5_directionalanisotropicfilter( im, kappa, lambda )
 %B.5 (extra credit): Implementation of a directional anisotropic filter
 %   (non-iterative form)
 %   from the paper "Fast anisotropic Gaussian filtering" by Geusebroek, et al
@@ -11,33 +12,53 @@ function [ filteredImage ] = b5_directionalanisotropicfilter( im, kappa, lambda 
 %Code References:
 %   mathworks.com/matlabcentral/fileexchange/31204-anisodiff-in-matlab/content/anisodiff.m
 %   mathworks.com/matlabcentral/fileexchange/14995-anisotropic-diffusion-perona-malik/content/anisodiff_Perona-Malik/anisodiff2D.m
+%   anigauss.* from Steger, et al. paper
+
+phi_radian = phi * pi / 180;
+% equation 11 from paper
+sig2_long = sig_long^2;
+sig2_lat = sig_lat^2;
+
+top = 0
+bottom = 0
+tanphi = top / bottom;
 
 I = double(im);
 [rows, cols] = size(I);
 filteredImage = I;
 
-% init filteredImageOperation with padding
-filteredImageOperation = zeros(rows+2, cols+2);
-filteredImageOperation(2:rows+1, 2:cols+1) = filteredImage;
+filtered_x = filter_aniso(x, y, sig_lat)
+filtered_y = filter_aniso(x, y, sig_long)
+filtered_t = filter_aniso(x, y, tanphi)
+filtered_derivative = filter_aniso(x, y, phi, order)
 
-% Differences
-diffN = filteredImageOperation(1:rows, 2:cols+1) - filteredImage;
-diffS = filteredImageOperation(3:rows+2, 2:cols+1) - filteredImage;
-diffE = filteredImageOperation(2:rows+1, 3:cols+2) - filteredImage;
-diffW = filteredImageOperation(2:rows+1, 1:cols) - filteredImage;
+gaussMask_x = getguasskernalmask(sig_long);
+gaussMask_y = getguasskernalmask(sig_lat);
 
-% Convolution Filter
-% Sheared filter
-% Filtered and interpolated
 
-% Conduction
-conductionN = 1 ./ (1 + (diffN/kappa).^2);
-conductionS = 1 ./ (1 + (diffS/kappa).^2);
-conductionE = 1 ./ (1 + (diffE/kappa).^2);
-conductionW = 1 ./ (1 + (diffW/kappa).^2);
 
-% Apply four-point-template
-filteredImage = filteredImage + lambda*(conductionN.*diffN + conductionS.*diffS + conductionE.*diffE + conductionW.*diffW);
+%% init filteredImageOperation with padding
+%filteredImageOperation = zeros(rows+2, cols+2);
+%filteredImageOperation(2:rows+1, 2:cols+1) = filteredImage;
+%
+%% Differences
+%diffN = filteredImageOperation(1:rows, 2:cols+1) - filteredImage;
+%diffS = filteredImageOperation(3:rows+2, 2:cols+1) - filteredImage;
+%diffE = filteredImageOperation(2:rows+1, 3:cols+2) - filteredImage;
+%diffW = filteredImageOperation(2:rows+1, 1:cols) - filteredImage;
+%
+%% Convolution Filter
+%% Sheared filter
+%% Filtered and interpolated
+%
+%% Conduction
+%conductionN = 1 ./ (1 + (diffN/kappa).^2);
+%conductionS = 1 ./ (1 + (diffS/kappa).^2);
+%conductionE = 1 ./ (1 + (diffE/kappa).^2);
+%conductionW = 1 ./ (1 + (diffW/kappa).^2);
+%
+%% Apply four-point-template
+%filteredImage = filteredImage + lambda*(conductionN.*diffN + conductionS.*diffS + conductionE.*diffE + conductionW.*diffW);
 
 figure('Name','Directional Anisotropic Filtered Image'),
 imshow(filteredImage, [])
--- project3/test_b5.m
+++ project3/test_b5.m
@@ -6,8 +6,17 @@ close all, clc
 
 %===============================================================================
 % Settings
-kappa = 100;
-lambda = 0.25;
+%old
+%kappa = 100;
+%lambda = 0.25;
+
+% For isotropic case, `sig_lat = sig_long`
+sig_long = 10;  % Sigma for longitudinal direction
+sig_lat = 5;    % Sigma for lateral direction
+
+% Degrees to use for answering question B.5:
+%   30, 60, 90, 120, 150
+angle = 30;   %phi
 %===============================================================================
 
 % User's image location settings
@@ -20,5 +29,7 @@ imagePath = [inputDir1, filesep, filename];     % Cross-platform format
 I = imread(imagePath);
 figure('Name','Original Image'), imshow(I, [])
 
-filteredImage = b5_directionalanisotropicfilter(I, kappa, lambda);
+filteredImage = b5_directionalanisotropicfilter(I, sig_long, sig_lat, angle);
+disp('Finished: B.5')
+%filteredImage = b5_directionalanisotropicfilter(I, kappa, lambda);
 % imshow() is inside the b5_directionalanisotropicfilter() function
