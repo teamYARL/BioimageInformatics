0a51ac9fe39c3bbd65de6b42ba74f772a438037d
ruanyuxian <ruanyuxian2008@gmail.com>
2014-04-07 00:03:48 -0400

    update the code

--- project3/test_c1.m
+++ project3/test_c1.m
@@ -4,11 +4,6 @@
 %===============================================================================
 close all%, clc
 
-%===============================================================================
-% Settings
-width = 14;        % find out why this in particular: width = ceil(3 * sigma
-sigma = 4.62;      % sigma >= width / sqrt(3) ~= 4.61880 if w=8
-
 % for smoothing
 %k = 100;
 %l = 0.25;
@@ -19,10 +14,17 @@ inputDir1 = 'curve_detection_images';
 filename = 'curv_det_01.tif';
 
 imagePath = [inputDir1, filesep, filename];     % Cross-platform format
-I = imread(imagePath);
-figure('Name','Original Image'), imshow(I, [])
+img = imread(imagePath);
+
+
+%===============================================================================
+% Settings
+sigma = ApplyStegersAlgor(img);   % calculate sigma by obtaining line width
+width = ceil(3*sigma);   % get the range for convolve
+
 
 curveDetectImage = I;
+
 %curveDetectImage = b5_directionalanisotropicfilter(curveDetectImage, k, l);
 % ordering of imshow() changed to compensate for xstarmatrix() plotting behavior
 figure('Name','C.1 : Curve Detection'), imshow(curveDetectImage, [])
