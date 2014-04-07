ad3b300c996c91f6f61df1a47f314c34e0331497
ruanyuxian <ruanyuxian2008@gmail.com>
2014-04-07 00:02:48 -0400

    change the code and crop 3 times to get the average of sigma

--- project3/ApplyStegersAlgor.m
+++ project3/ApplyStegersAlgor.m
@@ -1,11 +1,8 @@
-inputDir1 = 'curve_detection_images';%'microscope_char';
-%inputDir2 = 'DrosophilaVesicleTransport';
-filename = 'curv_det_01.tif';%'10x_calib.tif'; %'APPYFPX_Lgt_a07r10s01_001.tif';
-%outputDir = 'CroppedImages';
+% to get the sigma for gaussian function
+% img is the input image
+function [ sigma ] = ApplyStegersAlgor(img)
 
-imagePath = [inputDir1, filesep, filename]     % Cross-platform format
-I = imread(imagePath);
-I=double(I);
+I=double(img);
 figure('Name', 'Please select the region with the thickest line in this image.') 
 imshow(I, [])
 
@@ -16,17 +13,22 @@ croppedRegion = imcrop(I, rect);
 figure('Name', 'Please select the tight square around the widest curve in this image.') 
 imshow(croppedRegion, [])
 
-rect = getrect();
-croppedRegion = imcrop(croppedRegion, rect);
+data = [];
+% crop the line width for 3 times and calculate the average 
+for i = 1:3
+    rect = getrect();
+    area = imcrop(croppedRegion, rect);
+    s = size(area);
+    data = [data s(1)];
+end
 
-
-dimensionswidth=size(croppedRegion)
-width=mean(dimensionswidth)
-
+% get the average of line width
+width=mean(data)
+% get sigma
 sigma=width/sqrt(3)
-fixedmask=getguasskernalmask(sigma);
-size(fixedmask)
 
+fixedmask=getguasskernalmask(sigma);
+% size(fixedmask)
 I2 = conv2(I,fixedmask, 'same');   
 
 imshow(I2, [])
