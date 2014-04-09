519b2dc8299fcb2216f8fcc7620a0136d7547711
Luigi Leung <lleung@cmu.edu>
2014-04-09 09:43:17 -0400

    Ok to submit; include report and presentation when submitting

--- project3/test_c2.m
+++ project3/test_c2.m
@@ -4,28 +4,9 @@
 %===============================================================================
 %close all, clc
 
-%===============================================================================
-% Assumes result from `test_c1` is not cleared
 
-% Settings
-%width = 14;
-%sigma = 4.62;      % sigma >= width / sqrt(3) ~= 4.61880 if w=8
-
-% for smoothing
-%k = 100;
-%l = 0.25;
 %===============================================================================
-
-%% User's image location settings
-%inputDir1 = 'curve_detection_images';
-%filename = 'curv_det_01.tif';
-%
-%imagePath = [inputDir1, filesep, filename];     % Cross-platform format
-%I = imread(imagePath);
-%figure('Name','Original Image'), imshow(I, [])
-%
-%===============================================================================
-% Diffusion method
+% Method with filtering x*
 %===============================================================================
 %curveDetectImage = I;
 %%curveDetectImage = b5_directionalanisotropicfilter(curveDetectImage, k, l);
@@ -33,7 +14,6 @@
 %figure('Name','C.1 : Curve Detection'), imshow(curveDetectImage, [])
 %[curveDetectImage, curveBinary] = xstarmatrix_general(curveDetectImage, width, sigma);
 %===============================================================================
-
 %figure('Name','curveBinary'), imshow(curveBinary, [])
 
 
@@ -63,16 +43,14 @@
 % Using reference Region Growth method
 %===============================================================================
 %joinedDotsImage = c2_reference_regiongrowing(curveBinary, xmin:xmin+width, ymin:ymin+height, width*height);
-%joinedDotsImage = c2_reference_segCroissRegion(0,curveBinary,6,95); %(6,95) = pixelSeed
-%joinedDotsImage = c2_reference_regiongrowing(curveBinary, 6, 95, 2);   % 10 pixel join
 
 
-%%Bad seed. Pixel joining text area only (upper left corner area).
+%%Bad seed example. Pixel joining text area only (upper left corner area).
 %joinedDotsImage = c2_reference_regiongrowing(curveDetectImage, 6, 95, 20);   % 10 pixel join
 
 %%This gets fuzzy path; require result from test_c1.m
 %%  This reference region growth code is by D. Kroon from University of Twente
-joinedDotsImage = c2_reference_regiongrowing(curveDetectImage, 131, 760, 128);   % 10 pixel join
-figure('Name','C.2 : joined dots via region growth'), imshow(joinedDotsImage, [])
+joinedDotsImage = c2_reference_regiongrowing(curveDetectImage, 131, 760, 128);
+figure('Name','C.2 : Pixel joining via region growth'), imshow(joinedDotsImage, [])
 
 disp('Finished')
