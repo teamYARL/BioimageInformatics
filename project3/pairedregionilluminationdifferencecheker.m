8be2134cc746fca8c7be43a3a530f04d728e07fe
yimingx <yimingx@andrew.cmu.edu>
2014-04-09 15:27:00 -0400

    Update pairedregionilluminationdifferencecheker.m

--- project3/pairedregionilluminationdifferencecheker.m
+++ project3/pairedregionilluminationdifferencecheker.m
@@ -1,9 +1,11 @@
+%Does the caculation for the matrix in B.3
 function [ averageimagedifference ] = pairedregionilluminationdifferencecheker(Region)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 
 %i decided a 10 by 10 region grid is good enough
+%this is where the dimensions of the subregions are caclculated
 squareparameter=10;
 nRows=squareparameter;
 nCols=squareparameter;
@@ -13,9 +15,9 @@ usedheight=dimensions(1)-mod(dimensions(1),nRows);
 usedwidth=dimensions(2)-mod(dimensions(2),nCols);
 subheight=usedheight/nRows;
 subwidth=usedwidth/nCols;
-
+% a celll matrix to hold all the sub regions
 subregions=cell(numsubregions,1);
-
+%the subregions are cropped and added to the cell matrix as seperate sub regions
 for j=1:nRows
     for i=1:nCols
         heightstart=(j-1)*subheight+1;
@@ -24,9 +26,11 @@ for j=1:nRows
     end
 end
 
+%number of comparisons
 numregioncomparisons=nchoosek(numsubregions,2);
 numofsigdifferences=0;
 
+%all the pairwise comparsions are done here and all the kstest2 results are summed here
 for i=1:numsubregions-1
     for j=(i+1):numsubregions
         %averageimagedifference=class(subregions{i})
@@ -36,8 +40,8 @@ for i=1:numsubregions-1
     end
 end
 
-numofsigdifferences;
-
+%numofsigdifferences;
+% this is where the illumination difference is returned and then turned into the illumination uniformity
 averageimagedifference=numofsigdifferences/numregioncomparisons;
 
 end
