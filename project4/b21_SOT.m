b9871a104c96e23fa3711bfc887f44620c84ccab
Alan-g-s <ashteyma@andrew.cmu.edu>
2014-05-04 00:26:26 -0400

    made avi for otsu method

--- project4/b21_SOT.m
+++ project4/b21_SOT.m
@@ -5,7 +5,8 @@ clc, close all
 %===============================================================================
 % Settings
 %===============================================================================
-numOfHistograms = 2;
+
+%numOfHistograms = 2;
 
 
 %===============================================================================
@@ -13,21 +14,25 @@ numOfHistograms = 2;
 %===============================================================================
 % load image and get the size of this image
 %filename = 'Blue0001.tif';
-filename = 'image01.tiff';
+%filename = 'image01.tiff';
+filename = '60x_02.tif';
 Img = double(imread(filename));
+maxintensity=max(Img(:))
 imgSize = size(Img);
-
 % show original image
 figure('Name','Original image'); imagesc(Img); colormap gray; axis off; axis equal;
 
-% in order to use matitk, we have to use 3D data
-% so we first build a 3D volume of two layers, and each layer
-% contains the original image
-D = zeros(imgSize(1),imgSize(2),2);
-D(1:imgSize(1),1:imgSize(2),1) = Img(1:imgSize(1),1:imgSize(2));
-D(1:imgSize(1),1:imgSize(2),2) = Img(1:imgSize(1),1:imgSize(2));
+for i=2:(maxintensity-2)/10:maxintensity
+    numOfHistograms = i
+    % in order to use matitk, we have to use 3D data
+    % so we first build a 3D volume of two layers, and each layer
+    % contains the original image
+    D = zeros(imgSize(1),imgSize(2),2);
+    D(1:imgSize(1),1:imgSize(2),1) = Img(1:imgSize(1),1:imgSize(2));
+    D(1:imgSize(1),1:imgSize(2),2) = Img(1:imgSize(1),1:imgSize(2));
 
-% implement segmentation using Otsu's method
-%b = matitk('SOT',[max(D(:))], double(D));
-b = matitk('SOT',[numOfHistograms], double(D));
-figure('Name','SOT segmented image'); imagesc(squeeze(b(:,:,1))); colormap gray; axis off; axis equal;
+    % implement segmentation using Otsu's method
+    %b = matitk('SOT',[max(D(:))], double(D));
+    b = matitk('SOT',[numOfHistograms], double(D));
+    figure('Name','SOT segmented image'); imagesc(squeeze(b(:,:,1))); colormap gray; axis off; axis equal;
+end
---
+++ project4/runSOTonimageseq.m
@@ -0,0 +1,46 @@
+% Project Assignment #3
+%===============================================================================
+% B.2 : Crop background and save cropped image sequentially witout compression
+%===============================================================================
+% User's image location settings
+inputDir1 = 'Mito_GFP_a01';
+%inputDir2 = 'DrosophilaVesicleTransport';
+filename = 'MitoGFP_LgtGal4_a01r01s02001.tif';
+outputDir = 'SOT_segmented_Images'
+
+%imagePath = [inputDir1, filesep, inputDir2, filesep, filename];     % Cross-platform format
+imagePath = [inputDir1, filesep, filename];     % Cross-platform format
+I = imread(imagePath);
+figure('Name', 'First image in Sequence to be segmented'), imshow(I, [])
+
+% Create directory if doesn't exist
+if ~exist(outputDir, 'dir')
+    mkdir(outputDir);
+end
+
+structOfImages = dir([inputDir1, filesep, '*.tif']);
+for i = 1:length(structOfImages)
+    filename = structOfImages(i).name;
+    filenameOut = ['SOT_SEG_', filename];
+    filePathOut = [outputDir, filesep, filenameOut];
+    
+    imagePath = [inputDir1, filesep, filename];
+    Img = double(imread(imagePath));
+    %if i==1
+        maxintensity=max(Img(:));
+        numOfHistograms=maxintensity
+        disp('ok!!!')
+    %end
+    imgSize = size(Img);
+    % in order to use matitk, we have to use 3D data
+    % so we first build a 3D volume of two layers, and each layer
+    % contains the original image
+    D = zeros(imgSize(1),imgSize(2),2);
+    D(1:imgSize(1),1:imgSize(2),1) = Img(1:imgSize(1),1:imgSize(2));
+    D(1:imgSize(1),1:imgSize(2),2) = Img(1:imgSize(1),1:imgSize(2));
+
+    % implement segmentation using Otsu's method
+    %b = matitk('SOT',[max(D(:))], double(D));
+    b = matitk('SOT',[numOfHistograms], double(D));
+    imwrite(squeeze(b(:,:,1)), filePathOut, 'tiff', 'Compression','none');
+end
